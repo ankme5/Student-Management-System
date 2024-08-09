28376580cdd42f77a91d2c2f376efc95a29fa652
Ankit Meshram <ankitmeshram56@gmail.com>
2024-08-09 16:23:57 +0530

    udpate depricated method with latest or jwt

--- backend/src/main/java/com/techeazy/backend/utils/JWTHelper.java
+++ backend/src/main/java/com/techeazy/backend/utils/JWTHelper.java
@@ -9,10 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Date;
 import java.util.function.Function;
 
